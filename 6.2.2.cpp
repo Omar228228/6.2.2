a7d3e466a48da4a0af1dcee6dfd43404f154dc2b
Omar228228 <goblock542@gmail.com>
2021-11-16 19:23:39 +0300

    first commit

---
+++ 6.2.2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void CreateArray(int* a, int n, int i) //masive
+{
+		a[i] = 15 + rand() % 71;
+		if (i < n - 1) {
+			CreateArray(a, n, i + 1);
+		}
+}
+
+void Sum(int* a, int n, int &S, int i) //search
+{
+	if (i % 2 == 1)
+	{
+		S += a[i];
+	}
+	if (i < n - 1)
+	{
+		Sum(a, n, S, i + 1);
+	}
+}
+
+void PrintArray(int* a, const int n, int S, int i) //output
+{
+	cout << a[i] << " ";
+	if (i < n - 1) {
+		PrintArray(a, n, S, i + 1);
+	}
+	else {
+		cout << endl;
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 10;
+	int a[n];
+	int S = 0, i=0;
+
+	CreateArray(a, n, i);
+	Sum(a, n, S, i);
+	PrintArray(a, n, S, i);
+	cout << "MediumAriphmetic = " << (S / n) << endl;
+
+	return 0;
+}
---
+++ UnitTest6.2.2/UnitTest6.2.2.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Юра\source\repos\6.2.2\6.2.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest622
+{
+	TEST_CLASS(UnitTest622)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int i = 0;
+			int S=0;
+			int a[n] = { 29, 22, 25, 71, 26, 20, 20, 44, 71, 18 };
+			Sum(a, n, S, i);
+			int res = S / n;
+			Assert::AreEqual(17, res);
+		}
+	};
+}
